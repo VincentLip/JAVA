c0b72080201949c46a8f721f934ed1c99aec143a
Lipinski Vincent <lipinskivincent@gmail.com>
2023-03-30 17:19:45 +0200

    Matrice

--- ExoTableau/src/main/java/org/example/matrice/Matrice2.java
+++ ExoTableau/src/main/java/org/example/matrice/Matrice2.java
@@ -4,15 +4,24 @@ public class Matrice2 {
 
     public static void matrice2(){
 
-        int mat [][] = new int[4][5];
+        int[][] mat ;
+        mat = new int[4][5];
         int count = 1;
+        int sum = 0;
+        int multiplication=1;
+
+
         for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
+            for (int j = 0; j < mat[i].length; j++) {
                 mat[i][j]= count;
                 count++;
+                sum += mat[i][j];
+                multiplication *= mat[i][j];
                 System.out.print(mat[i][j] + "\t");
             }
             System.out.println();
         }
+        System.out.println("La somme des valeurs de la matrice est de " + sum);
+        System.out.println("La multiplication des valeurs de la matrice est de " + multiplication);
     }
 }
